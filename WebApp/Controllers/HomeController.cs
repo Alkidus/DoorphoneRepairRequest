bcc36ded703e78e54ab6e66d22c5c98d64812097
Kirill <alkiddkv@gmail.com>
2021-10-19 22:59:36 +0300

    редактирование

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using WebApp.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 namespace WebApp.Controllers
 {
@@ -19,6 +21,8 @@ namespace WebApp.Controllers
         }
         public IActionResult AddAdress()
         {
+            SelectList dfSystems = new SelectList(db.DomofonSystems, "DomofonSystemType", "_Name");
+            ViewBag.AllDomofonSystems = dfSystems;
             return View();
         }
         [HttpGet]
@@ -81,8 +85,25 @@ namespace WebApp.Controllers
             }
             return NotFound();
         }
+        //IList<Student> studentList = new List<Student>() {
+        //            new Student(){ StudentID=1, StudentName="Steve", Age = 21 },
+        //            new Student(){ StudentID=2, StudentName="Bill", Age = 25 },
+        //            new Student(){ StudentID=3, StudentName="Ram", Age = 20 },
+        //            new Student(){ StudentID=4, StudentName="Ron", Age = 31 },
+        //            new Student(){ StudentID=5, StudentName="Rob", Age = 19 }
+        //        };
+        //// GET: Student
+        //public ActionResult Index()
+        //{
+        //    ViewBag.TotalStudents = studentList.Count();
 
+        //    return View();
+        //}
+        //IList<DomofonSystem> domofonSystemsList = new List<DomofonSystem>();
+        //public async Task<IActionResult> GetAllDomofonSystems()
+        //{
 
+        //}
 
 
 
