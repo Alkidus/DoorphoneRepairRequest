326138633eb05ec66bb60e81d8d7fa421e1641b2
Kirill <alkiddkv@gmail.com>
2021-10-20 13:47:08 +0300

    доработал DropDawnList!!!!!!!!!!!!!!!!!!!!!

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -19,10 +19,31 @@ namespace WebApp.Controllers
         {
             db = context;
         }
+
+        //public List<SelectListItem> GetDomofonSystems()
+        //{
+        //    List<SelectListItem> dfsytemsList = (from dfs in db.DomofonSystems.AsEnumerable()
+        //                                         select new SelectListItem
+        //                                         {
+        //                                             Text = dfs.DomofonSystemType,
+        //                                             Value = dfs.Id.ToString()
+        //                                         }).ToList();
+        //    return dfsytemsList;
+        //}
         public IActionResult AddAdress()
         {
-            SelectList dfSystems = new SelectList(db.DomofonSystems, "DomofonSystemType", "_Name");
-            ViewBag.AllDomofonSystems = dfSystems;
+            List<DomofonSystem> dfSystems = new List<DomofonSystem>();
+            foreach(var el in db.DomofonSystems)
+            {
+                dfSystems.Add(el);
+            }
+            ViewBag.AllDomofonSystems = new SelectList(dfSystems, "Id", "DomofonSystemType");
+            List<DomofonKey> dfKeys = new List<DomofonKey>();
+            foreach (var el in db.DomofonKeys)
+            {
+                dfKeys.Add(el);
+            }
+            ViewBag.AllDomofonKeys = new SelectList(dfKeys, "Id", "DomofonKeyType");
             return View();
         }
         [HttpGet]
@@ -35,6 +56,9 @@ namespace WebApp.Controllers
         [HttpPost]
         public async Task<IActionResult> AddAdress(Adress adress)
         {
+            //SelectList dfSystems = new SelectList(db.DomofonSystems, "DomofonSystemType", "_Name");
+            //ViewBag.AllDomofonSystems = dfSystems;
+            
             db.Adresses.Add(adress);
             await db.SaveChangesAsync();
             return RedirectToAction("Index");
@@ -85,25 +109,7 @@ namespace WebApp.Controllers
             }
             return NotFound();
         }
-        //IList<Student> studentList = new List<Student>() {
-        //            new Student(){ StudentID=1, StudentName="Steve", Age = 21 },
-        //            new Student(){ StudentID=2, StudentName="Bill", Age = 25 },
-        //            new Student(){ StudentID=3, StudentName="Ram", Age = 20 },
-        //            new Student(){ StudentID=4, StudentName="Ron", Age = 31 },
-        //            new Student(){ StudentID=5, StudentName="Rob", Age = 19 }
-        //        };
-        //// GET: Student
-        //public ActionResult Index()
-        //{
-        //    ViewBag.TotalStudents = studentList.Count();
 
-        //    return View();
-        //}
-        //IList<DomofonSystem> domofonSystemsList = new List<DomofonSystem>();
-        //public async Task<IActionResult> GetAllDomofonSystems()
-        //{
-
-        //}
 
 
 
